c5aeda3b629d4c74307ca2e55d242714bd3c5680
Mountaineer Ben <bentantillo@gmail.com>
2021-10-20 22:58:33 -0700

    built databse call

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+// COnnect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your mysql username,
+        user: 'root',
+        // your mysql password,
+        password: 'mysqlrocks99!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) =>{
+    console.log(rows);
+});
 
 //Deafault response for any other request (NOT FOUND)
 app.use((req, res) => {
